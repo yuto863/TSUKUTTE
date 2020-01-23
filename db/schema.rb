048de976458ac5158aeea63cbb61d7fc7ab88b21
Yuto Koga <yuuto863koga@yahoo.co.jp>
2020-01-23 05:13:50 +0000

    advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_013318) do
+ActiveRecord::Schema.define(version: 2020_01_23_040541) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_01_22_013318) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
